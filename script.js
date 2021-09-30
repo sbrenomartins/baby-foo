ef58ae30f2ee499e41379e423e16f18c7c2149d3
Breno M. Silva <breno@bussoladoinvestidor.com>
2021-09-30 18:03:19 -0300

    melhorias na tela inicial

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-var y = 11;
+function descobrirSexoDoBebe() {
+  document.getElementById("inicio").style = "display: none;";
+  document.getElementById("clock").style = "border: 1px solid black; width: 5em; height: 5em; border-radius: 3.5em;";
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+  var y = 11;
 
-  y -= 1;
+  // Update the count down every 1 second
+  var x = setInterval(function() {
 
-  // Display the result in the element with id="demo"
-  document.getElementById("timer").innerHTML = y;
+    y -= 1;
 
-  // If the count down is finished, write some text
-  if (y < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "\n\nAntonella";
-    document.getElementById("clock").classList.remove('clock');
-    document.getElementById("clock").classList.add('clock-2');
-    document.getElementById("container").classList.remove('container');
-    document.getElementById("container").classList.add('container-2');
-  }
-}, 1000);
+    // Display the result in the element with id="demo"
+    document.getElementById("timer").innerHTML = y;
+
+    // If the count down is finished, write some text
+    if (y < 0) {
+      clearInterval(x);
+      document.getElementById("timer").innerHTML = "\n\nHeitor";
+      document.getElementById("clock").classList.remove('clock');
+      document.getElementById("clock").classList.add('clock-2');
+      document.getElementById("container").classList.remove('container');
+      document.getElementById("container").classList.add('container-2');
+    }
+  }, 1000);
+}
